7d8fae610388cee8265766f06e55e7f48c124064
Roshni joseph <roshnijoseph1990@gmail.com>
2024-11-01 15:14:49 +0530

    changed image

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,10 +5,10 @@ import { Navbar, Nav, Button, Image } from 'react-bootstrap';
 
 const Navigation = () => {
   return (
-    <Navbar className="p-3 d-flex justify-content-between">
+    <Navbar className="p-3 justify-content-between">
 
      <Navbar.Brand href="#home">
-     <Image src="vite.jpeg" alt="Logo" height="40" />
+     <Image src="vite.jpeg" alt="Logo" height="40px" />
     </Navbar.Brand>
     <Nav className="ml-auto">
       <Nav.Link href="#mentor">Mentorship</Nav.Link>
